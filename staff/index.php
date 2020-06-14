1b91ddd7dba94f06e522c0e33f36196d5f804327
ubong-essien <52317595+ubong-essien@users.noreply.github.com>
2020-06-14 10:42:26 +0100

    added the staff dashboard

--- staff/index.php
+++ staff/index.php
@@ -1,19 +1,20 @@
 <?php
+// this index page will have some menu for 1=upload,2=view all ,3=logout,4 = explore all research works
 include('../includes/admin_header.php');
 ?>
-<div class="features-boxed" style="margin-top:-1px;background-color:#808080">
-        <div class="container">
+<div class="features-boxed" >
+        <div class="container" style="margin-top:15px;background-color:#808080">
             <div class="intro"></div>
-            <div class="row features" style="margin-top:0px;">
+            <div class="row features" style="">
 			<?php 
-			$r=getAllRecord($con,"dashboard_menu","Link_row=1 AND active=1","link_order",4);
+			$r=getAllRecord($con,"staff_dashboard_menu","Link_row=1 AND active=1","link_order",4);
 			while($row=mysqli_fetch_assoc($r)):
 			?>
                 <div class="col-md-3 col-sm-6 item">
-                    <div class="box" style="border-radius:10px;background-color:#ffffff">
-                        <i style="color:orange;" class="fa fa-<?php echo $row['Icon']?> fa-5x"></i>
-						
-                        <h2><strong><a href="<?= home_base_url()."users/".$row['Link']?>" style="color:black;display:block;text-decoration:none"><?= $row['Link_Name']?></a></strong> </h2></div>
+                        <div class="box" style="border-radius:10px;background-color:#ffffff;height:65%;">
+                            <i style="color:orange;" class="fa fa-<?php echo $row['Icon']?> fa-3x"></i>
+                            <h4><strong><a href="<?= home_base_url()."users/".$row['Link']?>" style="color:black;display:block;text-decoration:none"><?= $row['Link_Name']?></a></strong> </h4>
+                        </div>
                 </div>
                 <?php
 				endwhile;
@@ -21,20 +22,63 @@ include('../includes/admin_header.php');
                 
             </div>
 		<div class="row features" style="margin-top:-50px;">
-		<?php
-			$r=getAllRecord($con,"dashboard_menu","Link_row=2 AND active=1","link_order",4);
-			while($row=mysqli_fetch_assoc($r)):
-			?>
-                <div class="col-md-3 col-sm-6 item">
+	
+                <div class="col-md-12 col-sm-12 item">
                     <div class="box" style="border-radius:10px;background-color:#ffffff">
-                        <i style="color:orange;"class="fa fa-<?php echo $row['Icon'];?> fa-5x"></i>
+                        <div class="row">
+                            <div class="col-md-4 col-xs-12">
+                                <table class="table table-bordered table-hover">
+                                <tr>
+                                <td>Name: </td>
+                                <td>Ubong Essien</td>
+                                </tr>
+                                <tr>
+                                <td>Position: </td>
+                                <td>lecturer 1</td>
+                                </tr>
+                                <tr>
+                                <td>Email: </td>
+                                <td>email@e-mail.com:</td>
+                                </tr>
+                               
+                                </table>
+                            </div>
+                            <div class="col-md-8 col-xs-12">
+                            <p class="alert alert-primary">This shows a breakdown of research works by you</p>
+                            <div class="row">
+
+                                    <div class="col-md-3 col-sm-6 ">
+                                        <div class="box" style="border-radius:10px;background-color:#c0c0c0;height:;">
+                                        <h4><strong><a href="<?= home_base_url()."users/".$row['Link']?>" style="color:black;display:block;text-decoration:none">Local Journal</a></strong> </h4>
+                                    
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3 col-sm-6 ">
+                                        <div class="box" style="border-radius:10px;background-color:#c0c0c0;height:;">
+                                        <h4><strong><a href="<?= home_base_url()."users/".$row['Link']?>" style="color:black;display:block;text-decoration:none">Int'l Journal</a></strong> </h4>
+                                    
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3 col-sm-6 ">
+                                        <div class="box" style="border-radius:10px;background-color:#c0c0c0;height:;">
+                                        <h4><strong><a href="<?= home_base_url()."users/".$row['Link']?>" style="color:black;display:block;text-decoration:none">My Books</a></strong> </h4>
+                                    
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3 col-sm-6 ">
+                                        <div class="box" style="border-radius:10px;background-color:#c0c0c0;height:;">
+                                        <h4><strong><a href="<?= home_base_url()."users/".$row['Link']?>" style="color:black;display:block;text-decoration:none">Seninar</a></strong> </h4>
+                                    
+                                        </div>
+                                    </div>
+                                
+                            </div>
+                            </div>
+                        </div>
 						
-                        <h2><strong><a href="<?= home_base_url()."users/".$row['Link']?>" style="color:black;display:block;text-decoration:none"><?= $row['Link_Name']?></a></strong> </h2></div>
+                        
                 </div>
-                 <?php
-				endwhile;
-				?>
-                
+            
            </div>
         </div>
     </div>
